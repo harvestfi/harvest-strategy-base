31ed0669f7145447ec93a11ae232c3f3d9080e7f
CryptJS13 <cryptjs13@protonmail.com>
2024-03-14 17:34:39 +0100

    Add seamless recovery strategies

---
+++ contracts/strategies/seamless/SeamlessRecoveryMainnet_DAI.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "./SeamlessRecovery.sol";
+
+contract SeamlessRecoveryMainnet_DAI is SeamlessRecovery {
+
+  constructor() public {}
+
+  function initializeStrategy(
+    address _storage,
+    address _vault
+  ) public initializer {
+    address underlying = address(0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb);
+    address aToken = address(0x37eF72fAC21904EDd7e69f7c7AC98172849efF8e);
+    address debtToken = address(0x2733e1DA7d35c5ea3ed246ed6b613DC3dA97Ce2E);
+    address seam = address(0x1C7a460413dD4e964f96D8dFC56E7223cE88CD85);
+    SeamlessRecovery.initializeBaseStrategy(
+      _storage,
+      underlying,
+      _vault,
+      aToken,
+      debtToken,
+      seam,
+      750,
+      770,
+      1000,
+      true
+    );
+    rewardTokens = [seam];
+  }
+}
---
+++ test/seamless/recover/wsteth.js
@@ -0,0 +1,127 @@
+// Utilities
+const Utils = require("../../utilities/Utils.js");
+const {
+  impersonates,
+  setupCoreProtocol,
+  depositVault,
+} = require("../../utilities/hh-utils.js");
+
+const addresses = require("../../test-config.js");
+const BigNumber = require("bignumber.js");
+const IERC20 = artifacts.require("IERC20");
+
+//const Strategy = artifacts.require("");
+const Strategy = artifacts.require("SeamlessRecoveryMainnet_wstETH");
+
+// Developed and tested at blockNumber 11821280
+
+// Vanilla Mocha test. Increased compatibility with tools that integrate Mocha.
+describe("Arbitrum Mainnet Seamless Recovery wstETH", function() {
+  let accounts;
+
+  // external contracts
+  let underlying;
+
+  // external setup
+  let underlyingWhale = "0xAA1dB2F567D0B9955b1Da90bA787Ad251FdB67ff";
+  let seam = "0x1C7a460413dD4e964f96D8dFC56E7223cE88CD85";
+
+  // parties in the protocol
+  let governance;
+  let farmer1;
+
+  // numbers used in tests
+  let farmerBalance;
+
+  // Core protocol contracts
+  let controller;
+  let vault;
+  let strategy;
+
+  async function setupExternalContracts() {
+    underlying = await IERC20.at("0xc1CBa3fCea344f92D9239c08C0568f6F2F0ee452");
+    seam = await IERC20.at(seam);
+    console.log("Fetching Underlying at: ", underlying.address);
+  }
+
+  async function setupBalance(){
+    let etherGiver = accounts[9];
+    await web3.eth.sendTransaction({ from: etherGiver, to: underlyingWhale, value: 10e18});
+
+    farmerBalance = await underlying.balanceOf(underlyingWhale);
+    await underlying.transfer(farmer1, farmerBalance, { from: underlyingWhale });
+  }
+
+  before(async function() {
+    governance = addresses.Governance;
+    accounts = await web3.eth.getAccounts();
+
+    farmer1 = accounts[1];
+
+    // impersonate accounts
+    await impersonates([governance, underlyingWhale]);
+
+    let etherGiver = accounts[9];
+    await web3.eth.sendTransaction({ from: etherGiver, to: governance, value: 10e18});
+
+    await setupExternalContracts();
+    [controller, vault, strategy] = await setupCoreProtocol({
+      "existingVaultAddress": "0x94012F721e059748B986394EB5fE2820025F47Ff",
+      "strategyArtifact": Strategy,
+      "strategyArtifactIsUpgradable": true,
+      "upgradeStrategy": true,
+      "underlying": underlying,
+      "governance": governance,
+    });
+
+    // whale send underlying to farmers
+    await setupBalance();
+  });
+
+  describe("Happy path", function() {
+    it("Farmer should earn money", async function() {
+      let farmerOldBalance = new BigNumber(await underlying.balanceOf(farmer1));
+      await depositVault(farmer1, underlying, vault, farmerBalance);
+
+      let hours = 10;
+      let blocksPerHour = 3600;
+      let oldSharePrice;
+      let newSharePrice;
+
+      for (let i = 0; i < hours; i++) {
+        console.log("loop ", i);
+
+        oldSharePrice = new BigNumber(await vault.getPricePerFullShare());
+        await controller.doHardWork(vault.address, { from: governance });
+        newSharePrice = new BigNumber(await vault.getPricePerFullShare());
+
+        govSeam = new BigNumber(await seam.balanceOf(governance));
+        console.log("old shareprice: ", oldSharePrice.toFixed());
+        console.log("new shareprice: ", newSharePrice.toFixed());
+        console.log("growth: ", newSharePrice.toFixed() / oldSharePrice.toFixed());
+        console.log("governance SEAM balance:", govSeam.toFixed());
+
+        apr = (newSharePrice.toFixed()/oldSharePrice.toFixed()-1)*(24/(blocksPerHour/1800))*365;
+        apy = ((newSharePrice.toFixed()/oldSharePrice.toFixed()-1)*(24/(blocksPerHour/1800))+1)**365;
+
+        console.log("instant APR:", apr*100, "%");
+        console.log("instant APY:", (apy-1)*100, "%");
+
+        await Utils.advanceNBlock(blocksPerHour);
+      }
+      await vault.withdraw(new BigNumber(await vault.balanceOf(farmer1)).toFixed(), { from: farmer1 });
+      let farmerNewBalance = new BigNumber(await underlying.balanceOf(farmer1));
+      Utils.assertBNGte(farmerNewBalance, farmerOldBalance);
+
+      apr = (farmerNewBalance.toFixed()/farmerOldBalance.toFixed()-1)*(24/(blocksPerHour*hours/1800))*365;
+      apy = ((farmerNewBalance.toFixed()/farmerOldBalance.toFixed()-1)*(24/(blocksPerHour*hours/1800))+1)**365;
+
+      console.log("earned!");
+      console.log("APR:", apr*100, "%");
+      console.log("APY:", (apy-1)*100, "%");
+
+      await strategy.withdrawAllToVault({from:governance}); // making sure can withdraw all for a next switch
+
+    });
+  });
+});
