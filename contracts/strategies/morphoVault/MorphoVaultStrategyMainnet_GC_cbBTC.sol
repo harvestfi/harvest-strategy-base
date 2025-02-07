20776590887f406d2c686ff75e0a6bc753128006
CryptJS13 <cryptjs13@protonmail.com>
2025-02-07 16:38:34 +0100

    Add morpho strategies

---
+++ contracts/strategies/morphoVault/MorphoVaultStrategyMainnet_GC_cbBTC.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.6.12;
+
+import "./MorphoVaultStrategy.sol";
+
+contract MorphoVaultStrategyMainnet_GC_cbBTC is MorphoVaultStrategy {
+
+  constructor() public {}
+
+  function initializeStrategy(
+    address _storage,
+    address _vault
+  ) public initializer {
+    address underlying = address(0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf);
+    address morphoVault = address(0x6770216aC60F634483Ec073cBABC4011c94307Cb);
+    address weth = address(0x4200000000000000000000000000000000000006);
+    address morpho = address(0xBAa5CC21fd487B8Fcc2F632f3F4E8D37262a0842);
+    MorphoVaultStrategy.initializeBaseStrategy(
+      _storage,
+      underlying,
+      _vault,
+      morphoVault,
+      weth
+    );
+    rewardTokens = [morpho];
+  }
+}
---
+++ contracts/strategies/morphoVault/MorphoVaultStrategyMainnet_SE_cbBTC.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.6.12;
+
+import "./MorphoVaultStrategy.sol";
+
+contract MorphoVaultStrategyMainnet_SE_cbBTC is MorphoVaultStrategy {
+
+  constructor() public {}
+
+  function initializeStrategy(
+    address _storage,
+    address _vault
+  ) public initializer {
+    address underlying = address(0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf);
+    address morphoVault = address(0x5a47C803488FE2BB0A0EAaf346b420e4dF22F3C7);
+    address weth = address(0x4200000000000000000000000000000000000006);
+    address seam = address(0x1C7a460413dD4e964f96D8dFC56E7223cE88CD85);
+    address morpho = address(0xBAa5CC21fd487B8Fcc2F632f3F4E8D37262a0842);
+    MorphoVaultStrategy.initializeBaseStrategy(
+      _storage,
+      underlying,
+      _vault,
+      morphoVault,
+      weth
+    );
+    rewardTokens = [seam, morpho];
+  }
+}
