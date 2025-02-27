78d4fe52bab0995ed823ee1ffd75a2f2f9c14e5b
ryanycw <ryan@ryanycw.dev>
2025-02-27 11:51:56 +0800

    refactor: enhance Setters contract with governance controls and factor management

--- contracts/strategies/morpho/utils/Setters.sol
+++ contracts/strategies/morpho/utils/Setters.sol
@@ -1,18 +1,37 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.26;
 
-import {ConstantsLib} from "../libraries/ConstantsLib.sol";
-import {BaseUpgradeableStrategyStorage} from "../../../base/upgradability/BaseUpgradeableStrategyStorage.sol";
+import {ControllableInit} from "../../../base/inheritance/ControllableInit.sol";
+import {MLSConstantsLib} from "../libraries/MLSConstantsLib.sol";
+import {Checks} from "./Checks.sol";
 
-abstract contract Setters {
-    function _setAddress(bytes32 slot, address _address) internal {
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            sstore(slot, _address)
-        }
+abstract contract Setters is ControllableInit, Checks {
+    function _setMToken(address _target) internal {
+        setAddress(MLSConstantsLib.MTOKEN_SLOT, _target);
     }
 
-    function _setMToken(address _target) internal {
-        _setAddress(ConstantsLib.MTOKEN_SLOT, _target);
+    function _setFactorDenominator(uint256 _denominator) internal {
+        setUint256(MLSConstantsLib.FACTORDENOMINATOR_SLOT, _denominator);
+    }
+
+    // updating collateral factor
+    // note 1: one should settle the loan first before calling this
+    // note 2: collateralFactorDenominator is 1000, therefore, for 20%, you need 200
+    function setCollateralFactorNumerator(uint256 _numerator) public onlyGovernance {
+        require(_numerator <= getFactorDenominator(), "Collateral factor cannot be this high");
+        require(
+            _numerator > getBorrowTargetFactorNumerator(),
+            "Collateral factor should be higher than borrow target"
+        );
+        setUint256(MLSConstantsLib.COLLATERALFACTORNUMERATOR_SLOT, _numerator);
+    }
+
+    function setFold(bool _fold) public onlyGovernance {
+        setBoolean(MLSConstantsLib.FOLD_SLOT, _fold);
+    }
+
+    function setBorrowTargetFactorNumerator(uint256 _numerator) public onlyGovernance {
+        require(_numerator < getCollateralFactorNumerator(), "Target should be lower than collateral limit");
+        setUint256(MLSConstantsLib.BORROWTARGETFACTORNUMERATOR_SLOT, _numerator);
     }
 }
