faaf4218d4972a732b3feed7fdc59ca6f6f06aa2
ryanycw <ryan@ryanycw.dev>
2025-02-27 11:52:19 +0800

    feat: extend Checks contract with unsalvagable tokens check

--- contracts/strategies/morpho/utils/Checks.sol
+++ contracts/strategies/morpho/utils/Checks.sol
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.26;
 
-abstract contract Checks {
+import {StateAccessor} from "./StateAccessor.sol";
+
+abstract contract Checks is StateAccessor {
+    function unsalvagableTokens(address token) public view returns (bool) {
+        return (
+            token == rewardToken() || token == underlying()
+                || token == getMToken()
+        );
+    }
+
     function depositArbCheck() public pure returns (bool) {
         // there's no arb here.
         return true;
