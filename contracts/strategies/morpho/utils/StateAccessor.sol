0710896f6e34fc11d349576cb86b5795c670c2c7
ryanycw <ryan@ryanycw.dev>
2025-03-02 00:28:59 +0800

    feat: add getMarketParams method to StateAccessor for Morpho Blue market configuration

--- contracts/strategies/morpho/utils/StateAccessor.sol
+++ contracts/strategies/morpho/utils/StateAccessor.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.26;
 
+import {MarketParams} from "@morpho-org/morpho-blue/src/interfaces/IMorpho.sol";
+
 import {MLSConstantsLib} from "../libraries/MLSConstantsLib.sol";
 import {BaseUpgradeableStrategyStorage} from "../../../base/upgradability/BaseUpgradeableStrategyStorage.sol";
 
@@ -21,6 +23,16 @@ abstract contract StateAccessor is BaseUpgradeableStrategyStorage {
 
     function getLLTV() public view returns (uint256) {
         return getUint256(MLSConstantsLib.LLTV_SLOT);
+    }
+
+    function getMarketParams() public view returns (MarketParams memory) {
+        return MarketParams({
+            loanToken: getLoanToken(),
+            collateralToken: underlying(),
+            oracle: getOracle(),
+            irm: getIRM(),
+            lltv: getLLTV()
+        });
     }
 
     /* MOONWELL */
