15fda35bbe7f3781d9e0fb2ea4f70c46e7b9d9fd
ryanycw <ryan@ryanycw.dev>
2025-03-26 23:56:39 +0800

    feat: add FlashLoanType enum to IMorphoLoopingStrategy for enhanced flash loan management

--- contracts/strategies/morpho/interfaces/IMorphoLoopingStrategy.sol
+++ contracts/strategies/morpho/interfaces/IMorphoLoopingStrategy.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.26;
 
-interface IMorphoLoopingStrategy {}
+interface IMorphoLoopingStrategy {
+    enum FlashLoanType {
+        None,
+        Deposit,
+        Withdraw
+    }
+}
