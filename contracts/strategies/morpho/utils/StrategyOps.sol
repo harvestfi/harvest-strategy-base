ed856d60720c7c17039c92035d804bb7c50a2b43
ryanycw <ryan@ryanycw.dev>
2025-04-10 23:47:33 +0800

    feat: implement salvage function with error handling for non-salvageable tokens and add reward token management in StrategyOps

--- contracts/strategies/morpho/utils/StrategyOps.sol
+++ contracts/strategies/morpho/utils/StrategyOps.sol
@@ -5,18 +5,39 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IUniversalLiquidator} from "../../../base/interface/IUniversalLiquidator.sol";
 import {BaseUpgradeableStrategy} from "../../../base/upgradability/BaseUpgradeableStrategy.sol";
+import {ErrorsLib} from "../libraries/ErrorsLib.sol";
+import {Checks} from "./Checks.sol";
 
 /// @title StrategyOps
 /// @author Harvest Community Foundation
 /// @notice The StrategyOps contract.
 /// @dev This contract can be turned into an library in the future after BaseUpgradeableStrategy is modularized.
 
-abstract contract StrategyOps is BaseUpgradeableStrategy {
+abstract contract StrategyOps is BaseUpgradeableStrategy, Checks {
     using SafeERC20 for IERC20;
 
     /// @dev Reset on each upgrade
     address[] public rewardTokens;
 
+    /**
+     * @notice Salvages a token.
+     * @param recipient The recipient of the salvage.
+     * @param token The token to salvage.
+     * @param amount The amount of tokens to salvage.
+     * @dev To make sure that governance cannot come in and take away the coins
+     */
+    function salvage(address recipient, address token, uint256 amount) public onlyGovernance {
+        if (unsalvagableTokens(token)) revert ErrorsLib.TokenNotSalvagable(token);
+        IERC20(token).safeTransfer(recipient, amount);
+    }
+
+    /**
+     * @notice Liquidates rewards.
+     * @param _sell Whether to sell the rewards.
+     * @param _rewardToken The reward token.
+     * @param _universalLiquidator The universal liquidator.
+     * @param _underlying The underlying token.
+     */
     function _liquidateRewards(bool _sell, address _rewardToken, address _universalLiquidator, address _underlying)
         internal
     {
@@ -50,6 +71,10 @@ abstract contract StrategyOps is BaseUpgradeableStrategy {
         }
     }
 
+    /**
+     * @notice Adds a reward token.
+     * @param _token The token to add.
+     */
     function addRewardToken(address _token) public onlyGovernance {
         rewardTokens.push(_token);
     }
