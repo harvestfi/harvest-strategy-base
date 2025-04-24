6371465ef5f8e1562a6ef1feb005a659aa2444ee
CryptJS13 <cryptjs13@protonmail.com>
2025-04-24 15:09:19 +0200

    Update drip

--- contracts/base/Drip.sol
+++ contracts/base/Drip.sol
@@ -15,8 +15,8 @@ contract Drip is Controllable {
     using Address for address;
     using SafeMath for uint256;
 
-    event DripAdded(DripMode mode, address vault, uint256 perSecond);
-    event DripRemoved(DripMode mode, address vault, uint256 perSecond);
+    event DripAdded(DripMode mode, address vault, uint256 perSecond, uint256 endTime);
+    event DripRemoved(DripMode mode, address vault, uint256 perSecond, uint256 endTime);
     event Dripped(address vault, uint256 amount);
 
     enum DripMode { TokenAmount, FixedRate}
@@ -26,25 +26,27 @@ contract Drip is Controllable {
         address vault;
         uint256 perSecond;
         uint256 lastDripTime;
+        uint256 endTime;
     }
 
     DripInfo[] public drips;
 
     constructor(address _storage) Controllable(_storage) {}
 
-    function addDrip(DripMode _mode, address _vault, uint256 _perSecond) public onlyGovernance {
+    function addDrip(DripMode _mode, address _vault, uint256 _perSecond, uint256 _endTime) public onlyGovernance {
         drips.push(DripInfo({
             mode: _mode,
             vault: _vault,
             perSecond: _perSecond,
-            lastDripTime: block.timestamp
+            lastDripTime: block.timestamp,
+            endTime: _endTime
         }));
-        emit DripAdded(_mode, _vault, _perSecond);
+        emit DripAdded(_mode, _vault, _perSecond, _endTime);
     }
 
     function removeDrip(uint256 _dripIndex) public onlyGovernance {
         require(_dripIndex < drips.length, "Invalid index");
-        emit DripRemoved(drips[_dripIndex].mode, drips[_dripIndex].vault, drips[_dripIndex].perSecond);
+        emit DripRemoved(drips[_dripIndex].mode, drips[_dripIndex].vault, drips[_dripIndex].perSecond, drips[_dripIndex].endTime);
         drips[_dripIndex] = drips[drips.length - 1];
         drips.pop();
     }
@@ -52,26 +54,46 @@ contract Drip is Controllable {
     function drip(uint256 _dripIndex) public {
         require(_dripIndex < drips.length, "Invalid index");
         DripInfo storage dripInfo = drips[_dripIndex];
-        uint256 timePassed = block.timestamp.sub(dripInfo.lastDripTime);
-        if (timePassed > 0) {
-            address token = IERC4626(dripInfo.vault).asset();
-            uint256 amount;
+        if (dripInfo.endTime > block.timestamp) {
+            uint256 timePassed = block.timestamp.sub(dripInfo.lastDripTime);
+            if (timePassed > 0) {
+                uint256 rate = getCurrentRate(_dripIndex);
+                uint256 amount = rate.mul(timePassed).div(1e18);
+                address token = IERC4626(dripInfo.vault).asset();
+                uint256 balance = IERC20(token).balanceOf(address(this));
+                if (Math.min(amount, balance) > 0) {
+                    dripInfo.lastDripTime = block.timestamp;
+                    IERC20(token).safeTransfer(dripInfo.vault, Math.min(amount, balance));
+                    emit Dripped(dripInfo.vault, Math.min(amount, balance));
+                }
+            }
+        }
+    }
+
+    function getCurrentRate(uint256 _dripIndex) public view returns (uint256) {
+        require(_dripIndex < drips.length, "Invalid index");
+        DripInfo storage dripInfo = drips[_dripIndex];
+        if (dripInfo.endTime > block.timestamp) {
+            uint256 timeToEnd = dripInfo.endTime.sub(dripInfo.lastDripTime);
+            uint256 rate;
             if (dripInfo.mode == DripMode.TokenAmount) {
-                amount = dripInfo.perSecond.mul(timePassed);
+                rate = dripInfo.perSecond.mul(1e18);
             } else if (dripInfo.mode == DripMode.FixedRate) {
                 uint256 totalAssets = IERC4626(dripInfo.vault).totalAssets();
-                uint256 rate = dripInfo.perSecond.mul(timePassed);
-                amount = totalAssets.mul(rate).div(1e18);
+                rate = totalAssets.mul(dripInfo.perSecond);
             } else {
                 revert("Invalid drip mode");
             }
+            address token = IERC4626(dripInfo.vault).asset();
             uint256 balance = IERC20(token).balanceOf(address(this));
-            if (Math.min(amount, balance) > 0) {
-                dripInfo.lastDripTime = block.timestamp;
-                IERC20(token).safeTransfer(dripInfo.vault, Math.min(amount, balance));
-                emit Dripped(dripInfo.vault, Math.min(amount, balance));
+            uint256 maxRate = balance.mul(1e18).div(timeToEnd);
+            if (rate > maxRate) {
+                return maxRate;
+            } else {
+                return rate;
             }
         }
+        return 0;
     }
 
     function dripAll() public {
--- test/test-config.js
+++ test/test-config.js
@@ -14,6 +14,7 @@ module.exports = {
   "Reader": "0x061886648F40C3F38bAdF2f64705bFDF0F6B12b3",
   "ULOwner": "0x6a74649aCFD7822ae8Fb78463a9f2192752E5Aa2",
   "RewardPrePayMorpho": "0x1E905e572100134aFeEe01E88703cdbb0AdAF62c",
+  "Drip": "0x49E7AF6426D48B83B0Fa15ecF32DC622Fb411F34",
   "Incentives": {
     "GlobalIncentivesExecutor": "0xC69171Ab8722e511bc41B68bc7463Bd89Eff476b",
     "GlobalIncentivesHelper": "0x84891022D6F0d478C1AA59a336aAAF555b3dA093",
