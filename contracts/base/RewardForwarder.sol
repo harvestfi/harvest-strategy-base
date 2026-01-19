dd88f8fa25166875581e82ff2fb85eed55d460da
CryptJS13 <cryptjs13@protonmail.com>
2026-01-19 12:06:05 +0100

    Update reward forwarder

--- contracts/base/RewardForwarder.sol
+++ contracts/base/RewardForwarder.sol
@@ -30,6 +30,8 @@ contract RewardForwarder is Controllable {
     mapping(address => uint256) public pendingProfitShare;
     mapping(address => uint256) public pendingPlatformFee;
 
+    event SwapFailed(address indexed tokenIn, address indexed tokenOut, uint256 amount);
+
     modifier onlyHardWorkerOrGovernance() {
         require(
             IController(controller()).hardWorkers(msg.sender) || (msg.sender == governance()),
@@ -78,33 +80,53 @@ contract RewardForwarder is Controllable {
         }
     }
 
+    function notifyFeeAdmin(
+        address _token,
+        uint256 _profitSharingFee,
+        uint256 _platformFee
+    ) external onlyGovernance {
+        require(_token != address(0), "token=0");
+        pendingProfitShare[_token] = pendingProfitShare[_token].add(_profitSharingFee);
+        pendingPlatformFee[_token] = pendingPlatformFee[_token].add(_platformFee);
+
+        if (idxPlusOne[_token] == 0) {
+            pendingRewardTokens.push(_token);
+            idxPlusOne[_token] = pendingRewardTokens.length;
+        }
+    }
+
     function distributeFee(address token) external onlyHardWorkerOrGovernance {
         uint256 profitSharingFee = pendingProfitShare[token];
         uint256 platformFee = pendingPlatformFee[token];
 
-        require(
-            profitSharingFee > 0 || platformFee > 0,
-            "No pending fees for this token"
-        );
+        require(profitSharingFee > 0 || platformFee > 0, "No pending fees for this token");
 
-        // Reset pending fees before distribution to prevent re-entrancy issues
-        pendingProfitShare[token] = 0;
-        pendingPlatformFee[token] = 0;
+        (bool protocolSuccess, bool psSuccess) = _distributeFee(token, profitSharingFee, platformFee);
 
-        // Remove token from pendingRewardTokens array
-        uint256 idx = idxPlusOne[token];
-        if (idx != 0) {
-            uint256 last = pendingRewardTokens.length - 1;
-            uint256 i = idx - 1;
-            if (i != last) {
-                address moved = pendingRewardTokens[last];
-                pendingRewardTokens[i] = moved;
-                idxPlusOne[moved] = i + 1; // update moved index
-            }
-            pendingRewardTokens.pop();
-            idxPlusOne[token] = 0; // clear
+        if (protocolSuccess) {
+            pendingPlatformFee[token] = 0;
         }
-        _distributeFee(token, profitSharingFee, platformFee);
+        if (psSuccess) {
+            pendingProfitShare[token] = 0;
+        }
+
+        // Only remove from list if fully settled
+        if (pendingPlatformFee[token] == 0 && pendingProfitShare[token] == 0) {
+            uint256 idx = idxPlusOne[token];
+            if (idx != 0) {
+                uint256 i = idx - 1;
+                uint256 last = pendingRewardTokens.length - 1;
+
+                if (i != last) {
+                    address moved = pendingRewardTokens[last];
+                    pendingRewardTokens[i] = moved;
+                    idxPlusOne[moved] = i + 1;
+                }
+
+                pendingRewardTokens.pop();
+                idxPlusOne[token] = 0;
+            }
+        }
     }
 
     function distributeAllFees() external onlyHardWorkerOrGovernance {
@@ -113,21 +135,41 @@ contract RewardForwarder is Controllable {
             uint256 profitSharingFee = pendingProfitShare[token];
             uint256 platformFee = pendingPlatformFee[token];
 
-            if (
-                profitSharingFee > 0 || platformFee > 0
-            ) {
-                // Reset pending fees before distribution to prevent re-entrancy issues
-                pendingProfitShare[token] = 0;
-                pendingPlatformFee[token] = 0;
+            if (profitSharingFee > 0 || platformFee > 0) {
+                (bool protocolSuccess, bool psSuccess) =_distributeFee(token, profitSharingFee, platformFee);
 
-                _distributeFee(token, profitSharingFee, platformFee);
+                if (protocolSuccess) {
+                    pendingPlatformFee[token] = 0;
+                }
+                if (psSuccess) {
+                    pendingProfitShare[token] = 0;
+                }
             }
         }
-        for (uint256 i = 0; i < pendingRewardTokens.length; ) {
-            idxPlusOne[pendingRewardTokens[i]] = 0;
-            unchecked { ++i; }
+        uint256 j = 0;
+        while (j < pendingRewardTokens.length) {
+            address token = pendingRewardTokens[j];
+
+            if (
+                pendingPlatformFee[token] == 0 &&
+                pendingProfitShare[token] == 0
+            ) {
+                // remove token (swap & pop)
+                uint256 last = pendingRewardTokens.length - 1;
+                address moved = pendingRewardTokens[last];
+
+                if (j != last) {
+                    pendingRewardTokens[j] = moved;
+                    idxPlusOne[moved] = j + 1;
+                }
+
+                pendingRewardTokens.pop();
+                idxPlusOne[token] = 0;
+                // do NOT increment j here (we need to check the moved token)
+            } else {
+                j++;
+            }
         }
-        delete pendingRewardTokens;
     }
 
     /**
@@ -141,34 +183,43 @@ contract RewardForwarder is Controllable {
         address _token,
         uint256 _profitSharingFee,
         uint256 _platformFee
-    ) internal {
+    ) internal returns (bool protocolSuccess, bool psSuccess){
         address _controller = controller();
         address liquidator = IController(_controller).universalLiquidator();
         address _targetToken = IController(_controller).targetToken();
         address _protocolFeeReceiver = IController(_controller).protocolFeeReceiver();
         address _profitSharingReceiver = IController(_controller).profitSharingReceiver();
 
+        protocolSuccess = (_platformFee == 0);
+        psSuccess       = (_profitSharingFee == 0);
+
         if (_token == _targetToken && _platformFee > 0) {
             IERC20(_targetToken).safeTransfer(_protocolFeeReceiver, _platformFee);
+            protocolSuccess = true;
         }
 
         if (_token == iFARM && _profitSharingFee > 0) {
             IERC20(iFARM).safeTransfer(_profitSharingReceiver, _profitSharingFee);
+            psSuccess = true;
         }
 
-        uint256 toSwap = 0;
-        if (_token != _targetToken) toSwap += _platformFee;
-        if (_token != iFARM) toSwap += _profitSharingFee;
 
-        if (toSwap > 0) {
+        if (_platformFee > 0 && _token != _targetToken) {
             IERC20(_token).safeApprove(liquidator, 0);
-            IERC20(_token).safeApprove(liquidator, toSwap);
-
-            if (_platformFee > 0 && _token != _targetToken) {
-                IUniversalLiquidator(liquidator).swap(_token, _targetToken, _platformFee, 1, _protocolFeeReceiver);
+            IERC20(_token).safeApprove(liquidator, _platformFee);
+            try IUniversalLiquidator(liquidator).swap(_token, _targetToken, _platformFee, 1, _protocolFeeReceiver) {
+                protocolSuccess = true;
+            } catch {
+                emit SwapFailed(_token, _targetToken, _platformFee);
             }
-            if (_profitSharingFee > 0 && _token != iFARM) {
-                IUniversalLiquidator(liquidator).swap(_token, iFARM, _profitSharingFee, 1,  _profitSharingReceiver);
+        }
+        if (_profitSharingFee > 0 && _token != iFARM) {
+            IERC20(_token).safeApprove(liquidator, 0);
+            IERC20(_token).safeApprove(liquidator, _profitSharingFee);
+            try IUniversalLiquidator(liquidator).swap(_token, iFARM, _profitSharingFee, 1,  _profitSharingReceiver) {
+                psSuccess = true;
+            } catch {
+                emit SwapFailed(_token, iFARM, _profitSharingFee);
             }
         }
     }
