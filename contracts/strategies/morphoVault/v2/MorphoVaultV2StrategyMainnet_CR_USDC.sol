a6d5637b105709311d1b6316ac132f79ec58667b
CryptJS13 <cryptjs13@protonmail.com>
2026-01-30 11:13:43 +0100

    Update for morpho v2

---
+++ contracts/strategies/morphoVault/v2/MorphoVaultV2StrategyMainnet_CR_USDC.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.26;
+
+import "./MorphoVaultV2Strategy.sol";
+
+contract MorphoVaultStrategyMainnet_CR_USDC_V2 is MorphoVaultV2Strategy {
+
+  constructor() {}
+
+  function initializeStrategy(
+    address _storage,
+    address _vault
+  ) public initializer {
+    address underlying = address(0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913);
+    address morphoVault = address(0x116e1A65717A534B73EcB7d4F6543c65DBCd0E46);
+    address weth = address(0x4200000000000000000000000000000000000006);
+    address morpho = address(0xBAa5CC21fd487B8Fcc2F632f3F4E8D37262a0842);
+    MorphoVaultV2Strategy.initializeBaseStrategy(
+      _storage,
+      underlying,
+      _vault,
+      morphoVault,
+      weth
+    );
+    rewardTokens = [morpho];
+    _setDistributionTime(morpho, 172_800); // 48 hours
+  }
+}
